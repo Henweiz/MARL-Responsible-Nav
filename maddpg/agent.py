bd6bfc25a5fcb6e7e6a59cb59109d8be809832c8
henwei zeng <h.zeng-2@student.tudelft.nl>
2024-09-19 19:11:14 +0200

    Add MADDPG agent from Agilerl

---
+++ maddpg/agent.py
@@ -0,0 +1,249 @@
+import os
+import numpy as np
+import torch
+from pettingzoo.mpe import simple_spread_v3
+from tqdm import trange
+
+from agilerl.components.multi_agent_replay_buffer import MultiAgentReplayBuffer
+from agilerl.hpo.mutation import Mutations
+from agilerl.hpo.tournament import TournamentSelection
+from agilerl.utils.utils import create_population
+from agilerl.wrappers.pettingzoo_wrappers import PettingZooVectorizationParallelWrapper
+from agilerl.algorithms.maddpg import MADDPG
+
+class MADDPGAgent:
+    def __init__(self,
+        state_dim,
+        action_dim,
+        one_hot,
+        NET_CONFIG,
+        INIT_HP,
+        num_envs,
+        device,
+        HPO=True):
+        
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.one_hot = one_hot
+        self.NET_CONFIG = NET_CONFIG
+        self.INIT_HP = INIT_HP
+        if (self.INIT_HP["POP_SIZE"] < 1):
+            self.HPO = False
+        self.num_envs = num_envs
+        self.device = device
+        self.pop = []
+        self.HPO = HPO
+    
+        if self.HPO:
+            self.pop = create_population("MADDPG",
+                            self.state_dim,
+                            self.action_dim,
+                            self.one_hot,
+                            self.NET_CONFIG,
+                            self.INIT_HP,
+                            population_size=self.INIT_HP["POP_SIZE"],
+                            num_envs=self.num_envs,
+                            device=self.device)
+            
+            # Instantiate a tournament selection object (used for HPO)
+            self.tournament = TournamentSelection(
+                tournament_size=2,  # Tournament selection size
+                elitism=True,  # Elitism in tournament selection
+                population_size=self.INIT_HP["POP_SIZE"],  # Population size
+                eval_loop=1,  # Evaluate using last N fitness scores
+            )
+
+            # Instantiate a mutations object (used for HPO)
+            self.mutations = Mutations(
+                algo="MADDPG",
+                no_mutation=0.2,  # Probability of no mutation
+                architecture=0.2,  # Probability of architecture mutation
+                new_layer_prob=0.2,  # Probability of new layer mutation
+                parameters=0.2,  # Probability of parameter mutation
+                activation=0,  # Probability of activation function mutation
+                rl_hp=0.2,  # Probability of RL hyperparameter mutation
+                rl_hp_selection=[
+                    "lr",
+                    "learn_step",
+                    "batch_size",
+                ],  # RL hyperparams selected for mutation
+                mutation_sd=0.1,  # Mutation strength
+                agent_ids=self.INIT_HP["AGENT_IDS"],
+                arch=self.NET_CONFIG["arch"],
+                rand_seed=1,
+                device=self.device,
+            )
+        else:
+            agent = MADDPG(
+                state_dims=self.state_dim,
+                action_dims=self.action_dim,
+                one_hot=self.one_hot,
+                n_agents=self.INIT_HP["N_AGENTS"],
+                agent_ids=self.INIT_HP["AGENT_IDS"],
+                O_U_noise=self.INIT_HP["O_U_NOISE"],
+                expl_noise=self.INIT_HP["EXPL_NOISE"],
+                vect_noise_dim=self.num_envs,
+                mean_noise=self.INIT_HP["MEAN_NOISE"],
+                theta=self.INIT_HP["THETA"],
+                dt=self.INIT_HP["DT"],
+                index=0,
+                max_action=self.INIT_HP["MAX_ACTION"],
+                min_action=self.INIT_HP["MIN_ACTION"],
+                net_config=self.NET_CONFIG,
+                batch_size=self.INIT_HP["BATCH_SIZE"],
+                lr_actor=self.INIT_HP["LR_ACTOR"],
+                lr_critic=self.INIT_HP["LR_CRITIC"],
+                learn_step=self.INIT_HP["LEARN_STEP"],
+                gamma=self.INIT_HP["GAMMA"],
+                tau=self.INIT_HP["TAU"],
+                discrete_actions=self.INIT_HP["DISCRETE_ACTIONS"],
+                device=self.device
+            )
+            self.pop.append(agent)
+            
+        # Configure the multi-agent replay buffer
+        self.memory = MultiAgentReplayBuffer(
+            self.INIT_HP["MEMORY_SIZE"],
+            field_names=["state", "action", "reward", "next_state", "done"],
+            agent_ids=self.INIT_HP["AGENT_IDS"],
+            device=self.device,
+        )
+
+    def train(self, num_envs, evo_steps, learning_delay, env):
+        pop_episode_scores = []
+        total_steps = 0
+        for agent in self.pop:  # Loop through population
+            state, info = env.reset()
+            scores = np.zeros(num_envs)
+            completed_episode_scores = []
+            steps = 0
+            if self.INIT_HP["CHANNELS_LAST"]:
+                state = {
+                    agent_id: np.moveaxis(s, [-1], [-3])
+                    for agent_id, s in state.items()
+                }
+
+            for idx_step in range(evo_steps // num_envs):
+                agent_mask = info["agent_mask"] if "agent_mask" in info.keys() else None
+                env_defined_actions = (
+                    info["env_defined_actions"]
+                    if "env_defined_actions" in info.keys()
+                    else None
+                )
+
+                # Get next action from agent
+                cont_actions, discrete_action = agent.get_action(
+                    states=state,
+                    training=True,
+                    agent_mask=agent_mask,
+                    env_defined_actions=env_defined_actions,
+                )
+                if agent.discrete_actions:
+                    action = discrete_action
+                else:
+                    action = cont_actions
+
+                # Act in environment
+                next_state, reward, termination, truncation, info = env.step(action)
+
+                scores += np.sum(np.array(list(reward.values())).transpose(), axis=-1)
+                total_steps += num_envs
+                steps += num_envs
+
+                # Image processing if necessary for the environment
+                if self.INIT_HP["CHANNELS_LAST"]:
+                    next_state = {
+                        agent_id: np.moveaxis(ns, [-1], [-3])
+                        for agent_id, ns in next_state.items()
+                    }
+
+                # Save experiences to replay buffer
+                self.memory.save_to_memory(
+                    state,
+                    cont_actions,
+                    reward,
+                    next_state,
+                    termination,
+                    is_vectorised=True,
+                )
+
+                # Learn according to learning frequency
+                # Handle learn steps > num_envs
+                if agent.learn_step > num_envs:
+                    learn_step = agent.learn_step // num_envs
+                    if (
+                        idx_step % learn_step == 0
+                        and len(self.memory) >= agent.batch_size
+                        and self.memory.counter > learning_delay
+                    ):
+                        # Sample replay buffer
+                        experiences = self.memory.sample(agent.batch_size)
+                        # Learn according to agent's RL algorithm
+                        agent.learn(experiences)
+                # Handle num_envs > learn step; learn multiple times per step in env
+                elif (
+                    len(self.memory) >= agent.batch_size and self.memory.counter > learning_delay
+                ):
+                    for _ in range(num_envs // agent.learn_step):
+                        # Sample replay buffer
+                        experiences = self.memory.sample(agent.batch_size)
+                        # Learn according to agent's RL algorithm
+                        agent.learn(experiences)
+
+                state = next_state
+
+                # Calculate scores and reset noise for finished episodes
+                reset_noise_indices = []
+                term_array = np.array(list(termination.values())).transpose()
+                trunc_array = np.array(list(truncation.values())).transpose()
+                for idx, (d, t) in enumerate(zip(term_array, trunc_array)):
+                    if np.any(d) or np.any(t):
+                        completed_episode_scores.append(scores[idx])
+                        agent.scores.append(scores[idx])
+                        scores[idx] = 0
+                        reset_noise_indices.append(idx)
+                agent.reset_action_noise(reset_noise_indices)
+
+            agent.steps[-1] += steps
+            pop_episode_scores.append(completed_episode_scores)
+
+        # Tournament selection and population mutation
+        if self.HPO:
+            _, pop = self.tournament.select(self.pop)
+            self.pop = self.mutations.mutation(pop)
+
+        # Update step counter
+        for agent in self.pop:
+            agent.steps.append(agent.steps[-1])
+        
+        return total_steps, pop_episode_scores
+
+    def evaluate_agent(self, env, eval_steps=None, eval_loop=1):
+                # Evaluate population
+        fitnesses = [
+            agent.test(
+                env,
+                swap_channels=self.INIT_HP["CHANNELS_LAST"],
+                max_steps=eval_steps,
+                loop=eval_loop,
+            )
+            for agent in self.pop
+        ]
+        return fitnesses
+
+    
+    def save_checkpoint(self, path, filename):
+        os.makedirs(path, exist_ok=True)
+        save_path = os.path.join(path, filename)
+        elite, _ = self.tournament.select(self.pop)
+        elite.save_checkpoint(save_path)
+    
+    def load_checkpoint(self, path):
+        for agent in self.pop:
+            agent.load_checkpoint(path)
+    
+    def reached_max_steps(self, max_steps):
+        return np.greater([agent.steps[-1] for agent in self.pop], max_steps).all()
+    
+    def agents_steps(self):
+        return [agent.steps[-1] for agent in self.pop]
---
+++ maddpg/main.py
@@ -0,0 +1,114 @@
+import os
+import numpy as np
+import torch
+from pettingzoo.mpe import simple_spread_v3
+from tqdm import trange
+
+from agilerl.components.multi_agent_replay_buffer import MultiAgentReplayBuffer
+from agilerl.hpo.mutation import Mutations
+from agilerl.hpo.tournament import TournamentSelection
+from agilerl.utils.utils import create_population
+from agilerl.wrappers.pettingzoo_wrappers import PettingZooVectorizationParallelWrapper
+
+from agent import MADDPGAgent
+
+if __name__ == '__main__':
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # Define the network configuration
+    NET_CONFIG = {
+        "arch": "mlp",  # Network architecture
+        "hidden_size": [32, 32],  # Actor hidden size
+    }
+
+    # Define the initial hyperparameters
+    INIT_HP = {
+        # Swap image channels dimension from last to first [H, W, C] -> [C, H, W]
+        "CHANNELS_LAST": False,
+        "BATCH_SIZE": 32,  # Batch size
+        "O_U_NOISE": True,  # Ornstein Uhlenbeck action noise
+        "EXPL_NOISE": 0.1,  # Action noise scale
+        "MEAN_NOISE": 0.0,  # Mean action noise
+        "THETA": 0.15,  # Rate of mean reversion in OU noise
+        "DT": 0.01,  # Timestep for OU noise
+        "LR_ACTOR": 0.001,  # Actor learning rate
+        "LR_CRITIC": 0.001,  # Critic learning rate
+        "GAMMA": 0.95,  # Discount factor
+        "MEMORY_SIZE": 100000,  # Max memory buffer size
+        "LEARN_STEP": 100,  # Learning frequency
+        "TAU": 0.01,  # For soft update of target parameters
+        "POLICY_FREQ": 2,  # Policy frequnecy
+        "POP_SIZE": 4,  # Population size
+    }
+
+    num_envs = 8
+    # Define the simple spread environment as a parallel environment
+    env = simple_spread_v3.parallel_env(continuous_actions=False)
+    env = PettingZooVectorizationParallelWrapper(env, n_envs=num_envs)
+    env.reset()
+
+    # Configure the multi-agent algo input arguments
+    try:
+        state_dim = [env.observation_space(agent).n for agent in env.agents]
+        one_hot = True
+    except Exception:
+        state_dim = [env.observation_space(agent).shape for agent in env.agents]
+        one_hot = False
+    try:
+        action_dim = [env.action_space(agent).n for agent in env.agents]
+        INIT_HP["DISCRETE_ACTIONS"] = True
+        INIT_HP["MAX_ACTION"] = None
+        INIT_HP["MIN_ACTION"] = None
+    except Exception:
+        action_dim = [env.action_space(agent).shape[0] for agent in env.agents]
+        INIT_HP["DISCRETE_ACTIONS"] = False
+        INIT_HP["MAX_ACTION"] = [env.action_space(agent).high for agent in env.agents]
+        INIT_HP["MIN_ACTION"] = [env.action_space(agent).low for agent in env.agents]
+
+    # Not applicable to MPE environments, used when images are used for observations (Atari environments)
+    if INIT_HP["CHANNELS_LAST"]:
+        state_dim = [
+            (state_dim[2], state_dim[0], state_dim[1]) for state_dim in state_dim
+        ]
+
+    # Append number of agents and agent IDs to the initial hyperparameter dictionary
+    INIT_HP["N_AGENTS"] = env.num_agents
+    INIT_HP["AGENT_IDS"] = env.agents
+
+    agents = MADDPGAgent(state_dim, action_dim, one_hot, NET_CONFIG, INIT_HP, num_envs, device, HPO=True)
+
+    # Define training loop parameters
+    max_steps = 1000000  # Max steps
+    learning_delay = 0  # Steps before starting learning
+
+    evo_steps = 10000  # Evolution frequency
+    eval_steps = None  # Evaluation steps per episode - go until done
+    eval_loop = 1  # Number of evaluation episodes
+
+    total_steps = 0
+
+    # TRAINING LOOP
+    print("Training...")
+    pbar = trange(max_steps, unit="step")
+    while not agents.reached_max_steps(max_steps):
+        steps, pop_episode_scores = agents.train(num_envs, evo_steps, learning_delay, env)
+        fitnesses = agents.evaluate_agent(env, eval_steps)
+        mean_scores = [
+            np.mean(episode_scores) if len(episode_scores) > 0 else 0.0
+            for episode_scores in pop_episode_scores
+        ]
+        total_steps += steps
+        pbar.update(evo_steps // len(agents.pop))
+
+        print(f"--- Global steps {total_steps} ---")
+        print(f"Steps {agents.agents_steps()}")
+        print(f"Scores: {mean_scores}")
+        print(f'Fitnesses: {["%.2f"%fitness for fitness in fitnesses]}')
+
+    
+    path = "./models/spread"
+    filename = "spread_MADDPG_trained_agent.pt"
+    agents.save_checkpoint(path, filename)
+    pbar.close()
+    env.close()
