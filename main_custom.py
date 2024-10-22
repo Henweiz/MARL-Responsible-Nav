af45acf60585fb2e95feff0267d9d9db1a0669c6
henwei zeng <h.zeng-2@student.tudelft.nl>
2024-10-22 12:36:44 +0200

    Clean up code

--- main_custom.py
+++ main_custom.py
@@ -2,6 +2,7 @@ import os
 #import imageio
 import numpy as np
 import torch
+import yaml
 
 from tqdm import trange
 from PIL import Image, ImageDraw
@@ -30,41 +31,23 @@ if __name__ == '__main__':
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # Define the initial hyperparameters
-    INIT_HP = {
-        # Swap image channels dimension from last to first [H, W, C] -> [C, H, W]
-        "CUSTOM_ENV": True,
-        "ARCH": "mlp",
-        "SEED": 42,
-        "CHANNELS_LAST": False,
-        "BATCH_SIZE": 128,  # Batch size
-        "O_U_NOISE": False,  # Ornstein Uhlenbeck action noise
-        "EXPL_NOISE": 0.15,  # Action noise scale
-        "MEAN_NOISE": 0.0,  # Mean action noise
-        "THETA": 0.15,  # Rate of mean reversion in OU noise
-        "DT": 0.01,  # Timestep for OU noise
-        "LR_ACTOR": 0.001,  # Actor learning rate
-        "LR_CRITIC": 0.001,  # Critic learning rate
-        "GAMMA": 0.98,  # Discount factor
-        "MEMORY_SIZE": 200000,  # Max memory buffer size
-        "LEARN_STEP": 10,  # Learning frequency
-        "TAU": 0.01,  # For soft update of target parameters
-        "POLICY_FREQ": 1,  # Policy frequnecy
-        "POP_SIZE": 1,  # Population size, 1 if we do not want to use Hyperparameter Optimization
-        "MAX_EPISODES": 3000,
-        "TRAIN_STEPS": 200,
+    LOGGING = {
         "LOAD_AGENT": False, # Load previous trained agent
-        "SAVE_AGENT": True, # Save the agent
-        "LOGGING": True,
+        "SAVE_AGENT": False, # Save the agent
+        "LOGGING": False,
         "RESUME": False,
-        "RESUME_ID": "rghnyha9",
-        "WITH_FEAR": False,
-        "FeAR_weight": -2.0,
-        "FeAR_trajectory_length": 5
+        "RESUME_ID": "3dze0erh"
     }
 
+    # Load YAML config file
+    with open('configs\custom.yaml', 'r') as file:
+        INIT_HP = yaml.safe_load(file)
+
+    INIT_HP.update(LOGGING)
+
     # Path & filename to save or load
-    path = "./models/custom/multi/MADDPG"
-    filename = "MADDPG_2.pt"
+    path = "./models/custom/multi/no_fear/MADDPG"
+    filename = "MADDPG_no_FeAR.pt"
 
     # Define the network configuration
     if INIT_HP["ARCH"] == "mlp":
@@ -87,32 +70,13 @@ if __name__ == '__main__':
     num_envs = 1
 
     # Define the simple spread environment as a parallel environment
-    #env = gym.make("intersection-multi-agent-v1", render_mode=None, config = config2)
-    #print(env.unwrapped.config)
-    #env = PettingZooVectorizationParallelWrapper(env, n_envs=num_envs)
-    env = CustomMAEnv(fear=INIT_HP["WITH_FEAR"])
-    obs, info = env.reset(INIT_HP["SEED"])
-    #env.num_agents = env.unwrapped.config['controlled_vehicles']
+    env = CustomMAEnv(fear=INIT_HP["WITH_FEAR"], seed=INIT_HP["SEED"])
+    obs, info = env.reset()
 
     env.agents = [f'agent_{i}' for i in range(env.num_agents)]
     # Logger
     if INIT_HP["LOGGING"]:
-        config = {
-            "Architecture:": NET_CONFIG["arch"],
-            "Hidden size": NET_CONFIG["hidden_size"],
-            "Batch size": INIT_HP["BATCH_SIZE"],
-            "Exploration noise": INIT_HP["EXPL_NOISE"],
-            "LR Actor": INIT_HP["LR_ACTOR"],
-            "LR Critic": INIT_HP["LR_CRITIC"],
-            "Discount": INIT_HP["GAMMA"],
-            "Memory size": INIT_HP["MEMORY_SIZE"],
-            "Learn step": INIT_HP["LEARN_STEP"],
-            "Train step": INIT_HP["TRAIN_STEPS"],
-            "Tau": INIT_HP["TAU"],
-            "Population size": INIT_HP["POP_SIZE"],
-            "WITH_FEAR": INIT_HP["WITH_FEAR"],
-            "FeAR_weight": INIT_HP["FeAR_weight"]
-        }
+        config = INIT_HP
         if INIT_HP["RESUME"]:
             logger = Logger(filename, config, id=INIT_HP["RESUME_ID"])
         else:
@@ -144,41 +108,34 @@ if __name__ == '__main__':
     INIT_HP["N_AGENTS"] = env.num_agents
     INIT_HP["AGENT_IDS"] = env.agents
 
-    agents = MADDPGAgent(state_dim, action_dim, one_hot, NET_CONFIG, INIT_HP, num_envs, device, HPO=True)
+    agents = MADDPGAgent(state_dim, action_dim, one_hot, NET_CONFIG, INIT_HP, num_envs, device)
 
     if INIT_HP["LOAD_AGENT"]:
         agents.load_checkpoint(path, filename)
         print("Agent succesfully loaded!")
 
     # Define training loop parameters
-    episodes = INIT_HP["MAX_EPISODES"]  # Max steps
+    episodes = INIT_HP["MAX_EPISODES"]  # Max episodes
     learning_delay = 0  # Steps before starting learning
 
-    evo_steps = INIT_HP["TRAIN_STEPS"]  # Evolution frequency
-    eval_steps = None  # Evaluation steps per episode - go until done
-    eval_loop = 1  # Number of evaluation episodes
+    env_steps = INIT_HP["TRAIN_STEPS"] 
 
-    total_steps = agents.pop[0].steps[-1]
+    total_steps = agents.agents_steps()
 
     # TRAINING LOOP
     print("Training...")
     pbar = trange(episodes, unit="episode")
     for i in range(episodes):
-        steps, pop_episode_scores, fear = agents.train(num_envs, evo_steps, learning_delay, env)
-        #fitnesses = agents.evaluate_agent(env, eval_steps)
-        mean_scores = [
-            np.mean(episode_scores) if len(episode_scores) > 0 else 0.0
-            for episode_scores in pop_episode_scores
-        ]
+        steps, returns, fear = agents.train(num_envs, env_steps, learning_delay, env)
 
         total_steps += steps
         pbar.update(1)
         if INIT_HP["LOGGING"]:
-            logger.log(np.mean(mean_scores), agents.total_loss(), steps, total_steps, fear)
+            logger.log(returns, agents.total_loss(), steps, total_steps, fear)
 
         print(f"--- Episode: {i} ---")
         print(f"Steps {steps}")
-        print(f"Scores: {mean_scores}")
+        print(f"Return: {returns}")
         print(f"Loss: {agents.total_loss()}")
         print(f'Fear: {fear}')
     
