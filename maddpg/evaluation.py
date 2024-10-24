72fd8cfa35a1b270ec68471f1e9c6a99d60d4296
Cem Levi <levicem@hotmail.com>
2024-10-24 14:16:05 +0200

    updated eval

--- maddpg/evaluation.py
+++ maddpg/evaluation.py
@@ -29,6 +29,15 @@ def make_dict(tuple, n_agents):
         dict[f'agent_{i}'] = tuple[i]
     return dict
 
+def relu(x):
+    """
+    Rectified Linear Unit (ReLU) activation function.
+    Args:
+    x (float or np.ndarray): Input to the ReLU function.
+    Returns:
+    float or np.ndarray: Output after applying ReLU.
+    """
+    return np.maximum(0, x) 
 
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -49,7 +58,7 @@ if __name__ == "__main__":
         "type": "MultiAgentObservation",
         "observation_config": {
             "type": "Kinematics",
-            "vehicles_count": 10,
+            "vehicles_count": 15,
             "features": ["presence", "x", "y", "vx", "vy", "cos_h", "sin_h"],
             "features_range": {
                 "x": [-100, 100],
@@ -62,7 +71,7 @@ if __name__ == "__main__":
     },
     "action": {"type": "MultiAgentAction",
                "action_config": {"type": "DiscreteMetaAction"}},
-    "initial_vehicle_count": 0,
+    "initial_vehicle_count": 5,
     "controlled_vehicles": 4,
     "collision_reward": -20,
     "high_speed_reward": 1,
@@ -147,86 +156,100 @@ if __name__ == "__main__":
 
     # Load the previous trained agent.
     path = os.path.join(path, filename)
-    agent.load_checkpoint(path)
+    paths = []
+    for path in paths:
+        agent.load_checkpoint(path)
 
 
-    #env = RecordVideo(
-    #    env, video_folder="intersection_maddpg/videos", episode_trigger=lambda e: True
-    #)
-    #env.unwrapped.set_record_video_wrapper(env)
-    env.unwrapped.config["simulation_frequency"] = 60  # Higher FPS for rendering
+        #env = RecordVideo(
+        #    env, video_folder="intersection_maddpg/videos", episode_trigger=lambda e: True
+        #)
+        #env.unwrapped.set_record_video_wrapper(env)
+        env.unwrapped.config["simulation_frequency"] = 60  # Higher FPS for rendering
 
-    num_crashes = []
-    num_arrivals = []
-    dist_to_dest = np.zeros((episodes,n_agents))
-    average_min_distance = np.zeros((episodes,n_agents))
-    
-    for videos in range(episodes):
+        num_crashes = []
+        num_arrivals = []
+        dist_to_dest = np.zeros((episodes,n_agents))
+        average_min_distance = np.zeros((episodes,n_agents))
         
-        average_min_distance_per_episode = []
-        
-        done = truncation = False
-        #state, info = env.reset(seed=seed)
-        state, info = env.reset()
-        
-        while not (done or truncation):
-            #print("step")
-            agent_mask = info["agent_mask"] if "agent_mask" in info.keys() else None
-            if net == "mlp":
-                state = [x.flatten() for x in state]
-                state_dict = make_dict(state, n_agents)
-            else:
-                state_dict = make_dict(state, n_agents)
-                state_dict = {
-                        agent_id: np.moveaxis(s, [-1], [-3])
-                        for agent_id, s in state_dict.items()
-                }
+        for videos in range(episodes):
             
-                # Get next action from agent
-            cont_actions, discrete_action = agent.get_action(
-                states=state_dict,
-                training=True,
-                agent_mask=agent_mask
-            )
-            if agent.discrete_actions:
-                action = discrete_action
-            else:
-                action = cont_actions
-
+            average_min_distance_per_episode = []
+            
+            done = truncation = False
+            #state, info = env.reset(seed=seed)
+            state, info = env.reset()
+            
+            while not (done or truncation):
+                #print("step")
+                agent_mask = info["agent_mask"] if "agent_mask" in info.keys() else None
+                if net == "mlp":
+                    state = [x.flatten() for x in state]
+                    state_dict = make_dict(state, n_agents)
+                else:
+                    state_dict = make_dict(state, n_agents)
+                    state_dict = {
+                            agent_id: np.moveaxis(s, [-1], [-3])
+                            for agent_id, s in state_dict.items()
+                    }
                 
-            # Act in environment
-            action_tuple  = tuple(action.values())
-            action_tuple = tuple(x.item() for x in action_tuple)
-            
-            next_state, _, _, truncation, info = env.step(action_tuple)
-            
-            reward = info["agents_rewards"]
-            termination = info["agents_terminated"]
-            termination_bad = info["agents_terminated_bad"]
-            termination_good = info["agents_terminated_good"]
-            nearest_vehicles = [env.unwrapped.road.close_objects_to(agent, env.PERCEPTION_DISTANCE, count=1, see_behind=False, sort=True, vehicles_only=True) for agent in env.controlled_vehicles]
-            min_distance_per_step = [np.linalg.norm(np.array(nearest_vehicle[0].position)) if nearest_vehicle != [] else 250 for nearest_vehicle in nearest_vehicles]
-            average_min_distance_per_episode.append(min_distance_per_step)
-            
-            state = next_state
-            
-            if all(termination_good) or any(termination_bad):
-                num_crashes.append(sum(termination_bad))
-                num_arrivals.append(sum(termination_good))
-                distances = np.array([torch.nn.functional.relu(torch.Tensor([25 - vehicle.lane.local_coordinates(vehicle.position)[0]])) for vehicle in env.controlled_vehicles if ("il" in vehicle.lane_index[0]
-            and "o" in vehicle.lane_index[1])])
-                if distances.shape[0] == 4:
+                    # Get next action from agent
+                cont_actions, discrete_action = agent.get_action(
+                    states=state_dict,
+                    training=True,
+                    agent_mask=agent_mask
+                )
+                if agent.discrete_actions:
+                    action = discrete_action
+                else:
+                    action = cont_actions
+
+                    
+                # Act in environment
+                action_tuple  = tuple(action.values())
+                action_tuple = tuple(x.item() for x in action_tuple)
+                
+                next_state, _, _, truncation, info = env.step(action_tuple)
+                
+                reward = info["agents_rewards"]
+                termination = info["agents_terminated"]
+                termination_bad = info["agents_terminated_bad"]
+                termination_good = info["agents_terminated_good"]
+                nearest_vehicles = [env.unwrapped.road.close_objects_to(agent, env.PERCEPTION_DISTANCE, count=1, see_behind=False, sort=True, vehicles_only=True) for agent in env.controlled_vehicles]
+                min_distance_per_step = [np.linalg.norm(np.array(nearest_vehicle[0].position)) if nearest_vehicle != [] else 250 for nearest_vehicle in nearest_vehicles]
+                average_min_distance_per_episode.append(min_distance_per_step)
+                
+                state = next_state
+                
+                if all(termination_good) or any(termination_bad):
+                    num_crashes.append(sum(termination_bad))
+                    num_arrivals.append(sum(termination_good))
+                    distances = np.array([relu(25 - vehicle.lane.local_coordinates(vehicle.position)[0]) for vehicle in env.controlled_vehicles ])
                     dist_to_dest[videos] = distances
-                average_min_distance[videos] = np.average(np.array(average_min_distance_per_episode), axis=0)
-                done = True
-            
-            # Render
-            #env.render()
-    env.close()
-    stat_dict = {"num_crashes": num_crashes, "num_arrivals": num_arrivals}
-    np.savetxt('average_min_distance.txt',average_min_distance)
-    np.savetxt("dist_to_dest.txt",dist_to_dest)
+                    average_min_distance[videos] = np.average(np.array(average_min_distance_per_episode), axis=0)
+                    done = True
+                
+                # Render
+                #env.render()
+        env.close()
+        
+        num_crashes = np.array(num_crashes)
+        num_arrivals = np.array(num_arrivals)
+        avg_crashes = np.average(num_crashes)
+        avg_arrivals = np.average(num_arrivals)
+        avg_distance_per_vec = np.average(dist_to_dest, axis=0)
+        avg_distance = np.average(avg_distance_per_vec)
+        avg_min_distance_per_vec = np.average(average_min_distance, axis=0)
+        avg_min_distance = np.average(avg_min_distance_per_vec)
+        
+        stat_dict = {}
+        name = path.split("w")[1].split(".")[0]
+        stat_dict[name] = {"avg_crashes":avg_crashes,"avg_arrivals": avg_arrivals,
+                        "avg_distance_per_vec": {f'agent{i}': x for i,x in enumerate(avg_distance_per_vec)},
+                        "avg_distance":avg_distance,"avg_min_distance_per_vec": {f'agent{i}': x for i,x in enumerate(avg_min_distance_per_vec)}, 
+                        "avg_min_distance":avg_min_distance}
+        
     with open('eval_stats.json', 'w') as convert_file: 
-     convert_file.write(json.dumps(stat_dict))
-    
+        convert_file.write(json.dumps(stat_dict))
+
 
